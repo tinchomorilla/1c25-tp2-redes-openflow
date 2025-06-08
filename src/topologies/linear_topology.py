c6179f3fa11500f871e80bae162418ee5ba1efd1
tinchomorilla <mmorilla@fi.uba.ar>
2025-06-08 00:30:55 -0300

    refactor LinearTopology to enhance build method and integrate remote controller for Mininet network setup

--- src/topologies/linear_topology.py
+++ src/topologies/linear_topology.py
@@ -1,41 +1,62 @@
 #!/usr/bin/env python2
 
 from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import RemoteController
+from mininet.log import setLogLevel
+from mininet.cli import CLI
 import argparse
+import os
+
 
 class LinearTopology(Topo):
-    def __init__(self, n_switches=3):
-        # Initialize topology
-        Topo.__init__(self)
-
-        # Add hosts
-        host1 = self.addHost('h1')
-        host2 = self.addHost('h2')
-
-        # Add switches
+    def build(self, n=3):
+        host1 = self.addHost("h1")
+        host2 = self.addHost("h2")
         switches = []
-        for i in range(n_switches):
-            switches.append(self.addSwitch('s%d' % (i+1)))
 
-        # Add links
-        # Connect host1 to first switch
+        # Crear switches
+        for i in range(n):
+            switch = self.addSwitch("s%d" % (i + 1))
+            switches.append(switch)
+
+        # Conectar host1 al primer switch
         self.addLink(host1, switches[0])
-        
-        # Connect switches in a chain
-        for i in range(n_switches - 1):
+
+        # Conectar switches en cadena
+        for i in range(n - 1):
             self.addLink(switches[i], switches[i + 1])
-        
-        # Connect last switch to host2
+
+        # Conectar último switch al host2
         self.addLink(switches[-1], host2)
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Create a linear topology with n switches')
-    parser.add_argument('n_switches', type=int, help='Number of switches in the chain')
+    parser = argparse.ArgumentParser(
+        description="Topología lineal con controlador remoto"
+    )
+    parser.add_argument("n", type=int, help="Cantidad de switches")
     args = parser.parse_args()
 
-    # Create topology
-    topo = LinearTopology(n_switches=args.n_switches)
-    print("Created linear topology with %d switches" % args.n_switches)
+    setLogLevel("info")  # Podés cambiar a 'debug' si querés más detalle
 
-if __name__ == '__main__':
+    topo = LinearTopology(n=args.n)
+
+    # Crear la red con controlador remoto (por default IP localhost)
+    net = Mininet(
+        topo=topo,
+        controller=lambda name: RemoteController(name, ip="127.0.0.1", port=6633),
+    )
+
+    net.start()
+    print("*** Red iniciada con %d switches" % args.n)
+    print("*** Ejecutá 'pingall' para probar conectividad o inspeccioná con Wireshark")
+    CLI(net)  # Entra en la CLI de Mininet
+    net.stop()
+
+    # Limpiar interfaces y procesos de Mininet
+    os.system("sudo mn -c")
+
+
+if __name__ == "__main__":
     main()
