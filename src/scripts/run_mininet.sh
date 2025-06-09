2ca4c4457cc2033547b7d0bd3465e13f256ae6e3
tinchomorilla <mmorilla@fi.uba.ar>
2025-06-08 23:46:40 -0300

    update run_mininet.sh to set DISPLAY environment variable and mount X11 socket for GUI support in Mininet container

--- src/scripts/run_mininet.sh
+++ src/scripts/run_mininet.sh
@@ -5,4 +5,4 @@ sudo docker-compose build mininet
 
 # Run Mininet container in a new terminal with proper environment
 # Using the container name 'sdn-project-mininet' as defined in docker-compose.yml
-gnome-terminal -- bash -c "cd $(pwd) && sudo docker-compose run --rm --name sdn-project-mininet -e PYTHONPATH=/app/src mininet; exec bash" 
+gnome-terminal -- bash -c "cd $(pwd) && sudo docker-compose run --rm --name sdn-project-mininet -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -e PYTHONPATH=/app/src mininet; exec bash" 
